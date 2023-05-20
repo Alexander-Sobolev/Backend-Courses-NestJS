063e46a7aba6b3ac8c78784733efead2ebbb9535
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-20 19:11:28 +0200

    Add controllers

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 1,
+    'prettier/prettier': 0,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": "off"
   },
 };
