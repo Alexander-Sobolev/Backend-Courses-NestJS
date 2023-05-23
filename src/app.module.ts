d0207ba73071c4ed00bc9932703f3168598ed880
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2023-05-23 18:00:28 +0200

    Add MongoConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './config/mongo.config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nestjs'),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }),
     ConfigModule.forRoot(),
     AuthModule, 
     TopPageModule, 
